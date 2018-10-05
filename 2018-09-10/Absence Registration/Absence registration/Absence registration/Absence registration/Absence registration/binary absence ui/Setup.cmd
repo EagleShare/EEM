ee5aaf026968c9c1d5ff544aef7511d4554e6bb7
EagleShare <36268143+EagleShare@users.noreply.github.com>
2018-10-05 17:47:39 +0530

    Delete Setup.cmd

--- 2018-09-10/Absence Registration/Absence registration/Absence registration/Absence registration/Absence registration/binary absence ui/Setup.cmd
+++
@@ -1,7 +0,0 @@
-
-set /p APIURL=Please Enter EEM API URL: 
-echo %APIURL%
-
-set filepath=%cd%\Absence_ui\static\js
-for %%F in ("%filepath%\*.js") do (set filename=%%~nxF)
-cscript replace.vbs "%filepath%\%filename%" "LABS_EEM_API_URL_" "%APIURL%"
